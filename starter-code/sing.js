02a32e928e12ba033f738afb5bdbd32ac78c35a4
Dan Finelli <dan.finelli@hotmail.com>
2016-11-01 16:40:44 -0700

    as far as i got in the beer song

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,23 @@
+
+var userInput = window.prompt('How many versus would you like?');
+userInput = Number(userInput);
+
+for (var i = userInput; i>-1; i--){
+
+  if(i>2){
+  alert(i+" bottles of beer on the wall,");
+  alert(i+" bottles of beer!");
+  alert("Take one down and pass it around,");
+  alert((i-1)+" bottles of beer on the wall!");
+} if (i===1) {
+    alert(i+" bottle of beer on the wall,");
+    alert(i+" bottle of beer!");
+    alert("Take one down and pass it around,");
+
+  } if (i === 0){
+    alert("No more bottles of beer on the wall!");
+  }
+
+}
+
 console.log("sing.js loaded");
