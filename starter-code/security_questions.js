835ddbaf19eb3685c7d80681aa1ce931a0f0547c
Dan Finelli <dan.finelli@hotmail.com>
2016-11-01 16:14:05 -0700

    actually that was security questions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,33 @@
+var securityQuestions = [];
+securityQuestions[0]={
+  question: "What was your first name?",
+  expectedAnswer: "Dan"};
+
+securityQuestions[1]={
+  question: "What was your first pet's name?",
+  expectedAnswer: "turtle"};
+
+securityQuestions[2]={
+  question: "What is your favorite make and model of a car?",
+  expectedAnswer: "chevy blazer"};
+
+var userAnswer = window.prompt(securityQuestions[0].question);
+
+if (userAnswer === securityQuestions[0].expectedAnswer){
+  userAnswer = window.prompt(securityQuestions[1].question);
+  if(userAnswer === securityQuestions[1].expectedAnswer){
+    userAnswer = window.prompt(securityQuestions[2].question);
+    if(userAnswer === securityQuestions[2].expectedAnswer){
+      console.log('congrats! youre in!');
+    } else {
+      console.log('incorrect');
+    }
+  } else {
+    console.log('incorrect');
+  }
+} else {
+  console.log('incorrect');
+
+}
+
 console.log("security_questions.js loaded");
